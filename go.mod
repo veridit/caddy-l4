0c400721f3c62147807fa4da4a4339cd71abbfe4
Jørgen H. Fjeld <jorgen@veridit.no>
2025-10-30 12:50:27 +0100

    WIP: Modify repo to allow install and build for testing before making PR

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/mholt/caddy-l4
+module github.com/veridit/caddy-l4
 
 go 1.25
 
--- imports.go
+++ imports.go
@@ -22,7 +22,7 @@ import (
 	_ "github.com/mholt/caddy-l4/modules/l4echo"
 	_ "github.com/mholt/caddy-l4/modules/l4http"
 	_ "github.com/mholt/caddy-l4/modules/l4openvpn"
-	_ "github.com/mholt/caddy-l4/modules/l4postgres"
+	_ "github.com/veridit/caddy-l4/modules/l4postgres"
 	_ "github.com/mholt/caddy-l4/modules/l4proxy"
 	_ "github.com/mholt/caddy-l4/modules/l4proxyprotocol"
 	_ "github.com/mholt/caddy-l4/modules/l4quic"
